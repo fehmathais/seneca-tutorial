c5edb9f3817985ba6afd03379722dcba8e4e88b9
Guilherme Maia <guilhermecmaia.io@gmail.com>
2018-09-17 22:57:04 -0300

    Create division and mulltiplication operations

---
+++ src/div.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = function div (msg, done) {
+  if (
+    (typeof msg.a === 'undefined' || typeof msg.a !== 'number') ||
+    (typeof msg.b === 'undefined' || typeof msg.b !== 'number')
+    ) {
+    return done(null, {
+      response: {
+        status: false,
+        message: 'A and B parameters are required numbers.'
+      }
+    });
+  }
+
+  return done(null, {
+    response: {
+      status: true,
+      result: (msg.a / msg.b)
+    }
+  })
+}
